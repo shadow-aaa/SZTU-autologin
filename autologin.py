f283263f09c7852dbffd55053ecd1bb8f48cd665
Colonos <114164230+shadow-aaa@users.noreply.github.com>
2023-09-03 10:05:32 +0800

    增加了base64加密
    2023年8月期间,北区联网系统更新了

--- autologin.py
+++ autologin.py
@@ -5,6 +5,7 @@ import socket  # 用于获得自己的IP
 from ping3 import ping  # 用于判断是否联网
 import pywifi   # 用于尝试并连接wifi
 import comtypes  # 似乎封装成EXE时需要这个模块
+import base64  # 用于base64加密
 
 
 def isConnected():  # 自动连接wifi
@@ -16,7 +17,7 @@ def isConnected():  # 自动连接wifi
 
 def ping_host(ip):  # 联网测试
     ip_address = ip
-    response = requests.get(ip_address,allow_redirects=False)
+    response = requests.get(ip_address, allow_redirects=False)
     return response.status_code
 
 
@@ -31,39 +32,38 @@ def get_host_ip():  # 变动IP获取
 
 
 # 需要输入数据的校园网登录界面
-url = 'http://47.98.217.39/lfradius/libs/portal/unify/portal.php/login/huawei_login'
-data = {
-    "usrname": "你的学号",   # 这行是你的学号,只有这两行需要你自己填
-    "passwd": "你的密码",      # 这行是你的密码
-    "treaty": "on",
-    "nasid": "3",
-    "usrmac": "30:5f:77:d9:28:01",  # 服务器的mac,也有可能是你自己的,不过我试了几次都是一样的mac
-    "usrip": get_host_ip(),  # 这个是你连上网后的IP地址,post包里唯一变动的参数
-    "basip": "172.17.127.254",
-    "success": "47.98.217.39/lfradius/libs/portal/unify/portal.php/login/success/nastype/huawei",
-    "fail": "47.98.217.39/lfradius/libs/portal/unify/portal.php/login/fail&offline=0",
-    "portal_version": "1",
-    "portal_papchap": "pap"
-}
+url = 'http://47.98.217.39/lfradius/libs/portal/unify/portal.php/login/cmcc_login'
+
+# 对data进行base64编码
+myip = get_host_ip()
+data = '{"usrname":"你的学号","passwd":"你的密码","usrmac":"30:e2:47:8a:f8:f7","usrip":"'+myip +\
+    '","basip":"172.17.127.254","nasid":"3","success":"http:\/\/47.98.217.39\/lfradius\/libs\/portal\/unify\/portal.php\/login\/success\/","fail":"http:\/\/47.98.217.39\/lfradius\/libs\/portal\/unify\/portal.php\/login\/fail\/","v":1,"t":"pap"}'
+encodedata = base64.b64encode(data.encode('utf-8'))
+finaldata = 'cmcc_login_value='+str(encodedata, 'utf-8')
+
+# 数据包请求头
 header = {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
     "Accept-Encoding": "gzip, deflate",
-    "Accept-Language": "en-US,en;q=0.9",
+    "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
     "Cache-Control": "max-age=0",
     "Connectin": "keep-alive",
+    "Content-Length": "376",
+    "Content-Type": "application/x-www-form-urlencoded",
     "Host": "47.98.217.39",
+    "Origin": "http://47.98.217.39",
     "Upgrade-Insecure-Requests": "1",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36 Edg/95.0.1020.53",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
 }
-# POST 方式向 URL 发送表单,实际上登录按钮就是发post数据包
+# POST 方式向 URL 发送数据包,实际上登录按钮就是发post数据包
 if __name__ == '__main__':
-    
-        # 断开之前连接,尝试连接SZTU-student
+
+    # 断开之前连接,尝试连接SZTU-student
     wifi = pywifi.PyWiFi()  # 创建一个无线对象
     ifaces = wifi.interfaces()[0]  # 取一个无限网卡
-    if(isConnected()==False):
-        # ifaces.disconnect()  # 断开网卡连接
-        # time.sleep(0.5)  # 缓冲0.5秒
+    if (isConnected() == False):
+        ifaces.disconnect()  # 断开网卡连接
+        time.sleep(0.5)  # 缓冲0.5秒
         profile = pywifi.Profile()  # 配置文件
         profile.ssid = "SZTU-student"  # wifi名称
         tmp_profile = ifaces.add_network_profile(profile)  # 加载配置文件
@@ -77,7 +77,7 @@ if __name__ == '__main__':
             if (ping_host('http://www.baidu.com') != 302):
                 print("您已登录,无需重复登录")
             else:
-                response = requests.post(url, data, headers=header)
+                response = requests.post(url, finaldata, headers=header)
                 time.sleep(1)
                 if (ping_host('http://www.baidu.com') != 302):
                     print("登录完毕,联网成功")
